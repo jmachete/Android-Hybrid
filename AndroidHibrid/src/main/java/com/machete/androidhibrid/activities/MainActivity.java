cfeeaf5596907ae21ffe89fc6bc7a2a3c3b1e418
Joao Machete <jmachete@gmail.com>
2013-10-28 17:34:33 -0200

    first commit

---
+++ AndroidHibrid/src/main/java/com/machete/androidhibrid/activities/MainActivity.java
@@ -0,0 +1,99 @@
+package com.machete.androidhibrid.activities;
+
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.machete.androidhibrid.R;
+
+/**
+ * Created by machete on 28/10/13.
+ */
+public class MainActivity extends ActionBarActivity {
+
+    private TextView mTextView;
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.textView);
+
+        webView = (WebView) findViewById(R.id.webView);
+        webView.addJavascriptInterface(new JavaScriptAccessor(), "AndroidFunction");
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/www/index.html");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public class JavaScriptAccessor {
+
+        public void getWebData(final String data) {
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    mTextView.setText(data);
+                }
+            });
+        }
+
+        public void showNativeToast(final String msg) {
+
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+
+                    mTextView.setText(msg);
+                }
+            });
+        }
+
+        public void openNativeDialog(final String msg) {
+
+            mTextView.setText(msg);
+
+            AlertDialog.Builder myDialog
+                    = new AlertDialog.Builder(MainActivity.this);
+            myDialog.setTitle("Dialog Box!");
+            myDialog.setMessage(msg);
+            myDialog.setPositiveButton("OK", null);
+            myDialog.show();
+
+        }
+
+
+        public void sendToJavascript() {
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    webView.loadUrl("javascript:callFromActivity(\""+123456789+"\")");
+                }
+            });
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,90 @@
+Android Hybrid allow you to build native mobile applications using HTML, CSS and JavaScript
+======
+
+###Objective
+Android Hybrid example that demonstrate a way to access native device function from javascript and vice-versa.
+
+###Structure
+
+* Web project should always be inside our `WebPlayer/src/main/assets/wwww`
+* Native code should always be inside our `WebPlayer/src/main/java`
+
+###The Mechanism
+######MyActivity.java
+```java
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main);
+  
+      WebView webView = (WebView) findViewById(R.id.webView);
+      webView.addJavascriptInterface(new JavaScriptAccessor(), "AndroidFunction");
+      webView.getSettings().setJavaScriptEnabled(true);
+      webView.loadUrl("file:///android_asset/www/index.html");
+  }
+  
+  public class JavaScriptAccessor {
+  
+      public void doSomethingNative() {
+          runOnUiThread(new Runnable() {
+              public void run() {
+                 //TODO: Do something here
+              }
+          });
+      }
+  }  
+```
+
+####Comunication: Web -> Native
+######Index.html
+```javascript
+  <script type="text/javascript">
+  
+      function setName(name) {
+          AndroidFunction.setName(name);
+      }
+  </script>
+```
+```html
+  <body>
+    <input type="button" class="button" value="Hello Telefonica" onClick="setName('Hello Android!')" />
+  </body>
+```
+######MyActivity.java
+```java
+  public class JavaScriptAccessor {
+  
+      public void setName(final String name) {
+          runOnUiThread(new Runnable() {
+              public void run() {
+                  nativeComponent.setText(name);
+              }
+          });
+      }
+  }
+```
+
+####Comunication: Native -> Web
+######Index.html
+```javascript
+  <script type="text/javascript">
+  
+    function callFromActivity(msg){
+        var element = document.getElementById("result");
+        element.innerHTML = msg;
+    }
+  </script>
+```
+```html
+  <body>
+    <div id="result"></div>
+  </body>
+```
+######MyActivity.java
+```java
+  runOnUiThread(new Runnable() {
+      public void run() {
+          webView.loadUrl("javascript:callFromActivity(\""+pausedTime+"\")");
+      }
+  });
+```
